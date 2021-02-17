7eac470f228e129c65320c7795d7d953e940d398
repl.it user <replituser@example.com>
2021-02-17 22:23:58 +0000

    removed node_modules

--- test.js
+++
@@ -1,9 +0,0 @@
-const Device = require("./");
-const fs = require("fs").promises;
-
-const reMarkable = new Device();
-
-(async () => {
-  await reMarkable.refresh(process.env.USER_TOKEN);
-  reMarkable.item("4c8566a1-1d89-4e3d-80e6-5f6bb125c5a7").then(console.log)
-})();
